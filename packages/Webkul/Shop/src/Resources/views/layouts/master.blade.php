a01dcfbaa3e1e6dfdd37236bdf54456027271c31
Pablo Martelletti <pmartelletti@gmail.com>
2022-07-01 10:29:39 +0200

    Add Meta Pixel and Google Analytics scripts.

--- packages/Webkul/Shop/src/Resources/views/layouts/master.blade.php
+++ packages/Webkul/Shop/src/Resources/views/layouts/master.blade.php
@@ -135,6 +135,35 @@
         {!! core()->getConfigData('general.content.custom_scripts.custom_javascript') !!}
     </script>
 
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '578285553686263');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=578285553686263&ev=PageView&noscript=1"
+        /></noscript>
+    <!-- End Meta Pixel Code -->
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y7C6KZ3GRT"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-Y7C6KZ3GRT');
+    </script>
+    <!-- End Google Analytics -->
+
 </body>
 
 </html>
